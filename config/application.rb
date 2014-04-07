0334e682b2554f562fc6bd0f620ce6a6f0397b4c
Kphillycat <keithwilliamsjr@gmail.com>
2014-04-07 09:42:09 -0400

    Add precompilation of assets via application.rb

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,8 @@ module MeetingSpot
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # config.assets.initialize_on_precompile = false
-    # config.assets.compress = true
+    config.assets.initialize_on_precompile = false
+    config.assets.compress = true
+    config.assets.precompile += ['jQuery.blockUI.js']
   end
 end
